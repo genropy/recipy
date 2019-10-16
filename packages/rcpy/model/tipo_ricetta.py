1f50c644522462169caf5e628de1e338659b2326
Francesco Porcari <francesco.porcari@softwell.it>
2019-10-16 19:25:24 +0200

    a

--- packages/rcpy/model/tipo_ricetta.py
+++ packages/rcpy/model/tipo_ricetta.py
@@ -3,6 +3,6 @@
 class Table(object):
     def config_db(self,pkg):
         tbl =  pkg.table('tipo_ricetta',pkey='id',name_long='Tipo Ricetta',name_plural='Tipi ricetta',caption_field='nome')
-        self.sysFields(tbl,hierarchical=True,counter=True)
+        self.sysFields(tbl,hierarchical='nome',counter=True)
         tbl.column('nome',name_long='Nome')
         tbl.column('descrizione',name_long='Descrizione')
