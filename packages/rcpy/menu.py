7704d5384a30ce7ef40ac7fa4e41be8c881863c8
Saverio Porcari <>
2019-10-18 15:05:01 +0200

    no message

--- packages/rcpy/menu.py
+++ packages/rcpy/menu.py
@@ -5,5 +5,4 @@ def config(root,application=None):
     rcpy = root.branch('Recipy')
     rcpy.thpage('Ingredienti',table='rcpy.ingrediente')
     rcpy.thpage('Ricette',table='rcpy.ricetta')
-    rcpy.thpage('Tipi ricetta',table='rcpy.ricetta_tipo')
     rcpy.lookups('Altre tabelle', lookup_manager='rcpy')
--- packages/rcpy/resources/tables/ricetta/th_ricetta.py
+++ packages/rcpy/resources/tables/ricetta/th_ricetta.py
@@ -28,7 +28,7 @@ class Form(BaseComponent):
         pane = bc.contentPane(region='top',datapath='.record')
         fb = pane.formbuilder(cols=3, border_spacing='4px', colswidth='auto', width='600px')
         fb.field('titolo', colspan=2, validate_notnull=True)
-        fb.field('ricetta_tipo_id', tag='hdbselect')
+        fb.field('ricetta_tipo_id')
         fb.field('descrizione', colspan=3, tag='simpleTextArea', height='10ex')
         fb.field('n_porzioni', width='5em', validate_notnull=True)
         fb.field('n_difficolta', tag='filteringSelect', values='3:Difficile,2:Media,1:Facile')
