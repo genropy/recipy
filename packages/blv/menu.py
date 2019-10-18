d21e0caff229304203b8c7384a9d0819134151a3
Saverio Porcari <>
2019-10-18 16:02:22 +0200

    no message

--- packages/blv/menu.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-
-def config(root,application=None):
-    blv = root.branch('Blv')
-    blv.thpage('Categorie',table='blv.categoria')
-    blv.thpage('Alimento',table='blv.alimento')
-    
-    
