617d44228fc0ee992a165527ab3699fe01ce105e
Saverio Porcari <>
2019-10-18 13:45:55 +0200

    menu

--- packages/rcpy/menu.py
+++ packages/rcpy/menu.py
@@ -4,4 +4,6 @@
 def config(root,application=None):
     rcpy = root.branch('Recipy')
     rcpy.thpage('Ingredienti',table='rcpy.ingrediente')
+    rcpy.thpage('Ricette',table='rcpy.ricetta')
+    rcpy.thpage('Tipi ricetta',table='rcpy.ricetta_tipo')
     rcpy.lookups('Altre tabelle', lookup_manager='rcpy')
